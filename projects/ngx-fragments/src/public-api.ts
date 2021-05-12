9ea3c9ec34e3a3f26c94a7b564165b478aa69708
mayinbun <bunyamin.gs@gmail.com>
2021-05-12 11:46:56 +0200

    add modules to entrypoint

--- projects/ngx-fragments/src/public-api.ts
+++ projects/ngx-fragments/src/public-api.ts
@@ -2,11 +2,10 @@
  * Public API
  */
 
-export * from './lib/fragments-factory.service';
-
-export * from './lib/fragments.service';
 export * from './lib/fragments-container.component';
 export * from './lib/fragments.module';
+export * from './lib/modules/fragments-root.module';
+export * from './lib/modules/fragments-feature.module';
 
 export * from './lib/fragment-outlet/fragment-outlet-base';
 export * from './lib/fragment-outlet/fragment-outlet.component';
